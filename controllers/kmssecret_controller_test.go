bce0cca4206369740b683fd0d07ebd854b9d7e8a
AkiraFukushima <h3.poteto@gmail.com>
2020-05-21 17:35:26 +0900

    Add a test to check registration

--- controllers/kmssecret_controller_test.go
+++ controllers/kmssecret_controller_test.go
@@ -1,11 +1,15 @@
 package controllers
 
 import (
+	"context"
 	"sync"
 	"time"
 
+	secretv1beta1 "github.com/h3poteto/kms-secrets/api/v1beta1"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
@@ -65,3 +69,50 @@ var _ = Describe("KMSSecretReconciler", func() {
 		mgrStopped.Wait()
 	})
 })
+
+var _ = Describe("Resource is created", func() {
+	// This is a dummy value.
+	encryptedPassword := "AQECAHhRyjfcAU0BFeAevHu12PEb8p++xXO5Ct1XvR/ARXpyaQAAAGYwZAYJKoZIhvcNAQcGoFcwVQIBADBQBgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDIG5VsUXqw3PTaFgcAIBEIAjEciyjvz5Rp+MAl0yH76o1EDP5N2qzmH8WkwgR3zRjyA8mcc="
+
+	It("should decrypt encrypted secret", func() {
+		_, err := manager.New(cfg, manager.Options{})
+		Expect(err).NotTo(HaveOccurred())
+
+		kmsSecret := &secretv1beta1.KMSSecret{
+			Spec: secretv1beta1.KMSSecretSpec{
+				Template: secretv1beta1.SecretTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-secret",
+						Namespace: "default",
+					},
+				},
+				EncryptedData: map[string][]byte{
+					"password": []byte(encryptedPassword),
+				},
+				Region: "ap-northeast-1",
+			},
+		}
+		kmsSecret.Name = "test-kms-secret"
+		kmsSecret.Namespace = "default"
+		ctx := context.Background()
+		err = k8sClient.Create(ctx, kmsSecret)
+		Expect(err).NotTo(HaveOccurred())
+
+		kmsSecret = getKMSSecret(ctx, "default", "test-kms-secret")
+		Expect(kmsSecret.Spec.EncryptedData).To(Equal(map[string][]byte{
+			"password": []byte(encryptedPassword),
+		}))
+		Expect(kmsSecret.ObjectMeta.UID).ToNot(Equal(""))
+	})
+})
+
+func getKMSSecret(ctx context.Context, namespace, name string) *secretv1beta1.KMSSecret {
+	kmsSecret := &secretv1beta1.KMSSecret{}
+	namespaced := types.NamespacedName{
+		Namespace: namespace,
+		Name:      name,
+	}
+	err := k8sClient.Get(ctx, namespaced, kmsSecret)
+	Expect(err).NotTo(HaveOccurred())
+	return kmsSecret
+}
